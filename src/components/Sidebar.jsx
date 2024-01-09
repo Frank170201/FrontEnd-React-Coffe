45b5fd6f2fbd94dab5d9d4476e148c356d3d6de7
Francisco Santamaria <sm19035@ues.edu.sv>
2024-01-08 21:46:56 -0600

    cambios

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import { categorias } from "../data/categorias";
+export default function Sidebar() {
+  return (
+    <aside className="md:w-72">
+      <div className="p-4">
+        <img className="w-40" src="../img/logo.svg" alt="" />
+      </div>
+      <div className="mt-10">
+        {categorias.map((categoria) => (
+          <p>{categoria.nombre}</p>
+        ))}
+      </div>
+    </aside>
+  );
+}
